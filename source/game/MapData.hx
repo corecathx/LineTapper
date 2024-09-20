d888aced21d26f360acc9638033e2e1f921a848f
ZSolarDev <101154324+ZSolarDev@users.noreply.github.com>
2024-09-19 22:55:25 -0400

    Add files via upload

--- source/game/MapData.hx
+++ source/game/MapData.hx
@@ -1,29 +1,29 @@
 package game;
 
+import objects.ArrowTile.TileColorData;
 import haxe.Json;
+import openfl.media.Sound;
 
+typedef MapAsset =
+{
+	var audio:Sound;
+	var map:LineMap;
+}
 typedef TileData = {
     var step:Int;
     var direction:Int;
 }
+typedef Theme = {
+    var tileColorData:TileColorData;
+    var bg:String;
+}
 typedef LineMap = {
     var tiles:Array<TileData>;
+    var theme:Theme;
     var bpm:Float;
 }
 
 class MapData {
-	// Ignore please
-    public static final TESTING_MAP:LineMap = {
-        tiles: [ // starts down
-            { step: 4, direction: 3 }, // right
-            { step: 6, direction: 2 }, // up
-            { step: 10, direction: 0 }, // left
-            { step: 12, direction: 1 }, // down
-            { step: 14, direction: 3 }, // right
-            { step: 16, direction: 2 }, // up
-        ],
-        bpm: 159
-    };
     public static function loadMap(rawJson:String):LineMap {
         return cast Json.parse(rawJson);
     }
